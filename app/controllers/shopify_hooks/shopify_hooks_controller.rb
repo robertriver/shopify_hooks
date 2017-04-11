745863af2b87c91bfce70c42f2e9b0432d09c2f7
Garrett Boone <garrettboone@dbc-garrettboone.local>
2017-04-11 14:56:21 -0500

    add .as_json b/c rails 5 and delyed job dont get along with passing ActiveSupport::Paramters

--- app/controllers/shopify_hooks/shopify_hooks_controller.rb
+++ app/controllers/shopify_hooks/shopify_hooks_controller.rb
@@ -11,14 +11,14 @@ module ShopifyHooks
     # end
 
     def create_refund
-      shopify_refund = params
+      shopify_refund = params.as_json
       user_email = shopify_refund["email"]
       ShopifyRefundWorker.perform_later(user_email,shopify_refund)
       render json: {status: 200}
     end
 
     def create_order
-      shopify_order = params
+      shopify_order = params.as_json
       user_email = shopify_order["email"]
       ShopifyOrderWorker.perform_later(user_email,shopify_order)
       render json: {status: 200}
@@ -26,14 +26,14 @@ module ShopifyHooks
 
     def create_product(new_or_updated_product = false)
       if !new_or_updated_product
-        new_or_updated_product = params
+        new_or_updated_product = params.as_json
       end
       ShopifyCreateProductWorker.perform_later(new_or_updated_product)
       render json: {status: 200}
     end
 
     def update_product
-      updated_product = params
+      updated_product = params.as_json
       found_product = Product.find_by(:shopify_product_id => updated_product["id"])
       if found_product
         ShopifyUpdateProductWorker.perform_later(updated_product, found_product)
